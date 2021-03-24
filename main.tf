09879eed0f5ac1f387459ad0d56419dfd24573a7
Anitha Edith <sanithaedith@yahoo.com>
2021-03-24 15:30:05 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "anitha-tel"
+
+    workspaces {
+      name = "terraform_test"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" tel_test {
+    bucket_prefix = "teltest-"
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+    type = string
+}
+variable "secret_key" {
+    type = string
+}
+
+github
+    sanithaedith@yahoo.com
+    Annie_1985
